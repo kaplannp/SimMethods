f07eb133e22226ea2fd78ff428bf4c79d3c257b1
kaplannp <kaplannp@whitman.edu>
2022-03-09 12:54:16 -0800

    Made damping!

--- Proj01/Part03/vibrating_string.py
+++ Proj01/Part03/vibrating_string.py
@@ -18,26 +18,7 @@ from scipy.optimize import minimize
 from scipy.optimize import differential_evolution
 
 
-def u(x, t, L, a, prec=1e-7):
-    '''
-    We don't know what this function does, but it is never called.
-    '''
-    x = np.asarray(x)
-    uu = np.zeros_like(x)
-    nn = 0
-    while True:
-        uu_ = ((-1) ** nn *
-               sin((2 * nn + 1) * pi * x / L) *
-               cos((2 * nn + 1) * pi * t / L) /
-               (2 * nn + 1) ** 2)
-        uu += uu_
-        if np.all(np.abs(uu_ / a) < prec):
-            break
-        nn += 1
-    uu *= 8 * a / pi**2
-    return uu
-
-def euler(t, dt, c, pos, vel):
+def euler(t, dt, c, gamma, pos, vel):
     '''
     This function executes a series of approximations using eulers method.
     It updates the state t/dt times at increments of dt.
@@ -46,25 +27,35 @@ def euler(t, dt, c, pos, vel):
       dt: steps in which to evaluate system over time to pass
       c: constant. The bigger the c, the bigger the change in velocity over the
         same sized timestep, dt
+      gamma: damping constant. Should be negative. If positive, you're exploding
       pos: array of positions of each unit on the string
       vel: array of velocities at each unit on the string
     Postconditions:
       pos and vel are updated
 
     '''
-
     for timestep in range(int(t/dt)):
+
+        #calculate first order derivative force contribution
         pos += vel*dt # update position based on the change in velocity
-        totalforce = np.zeros(pos.shape)
+        pos_totalforce = np.zeros(pos.shape)
         #offset by 1, so summing (n) = (n) - (n+1)
-        totalforce[:-1] += (pos[1:]-pos[:-1])
-        totalforce[-1] += -pos[-1]
+        pos_totalforce[:-1] += (pos[1:]-pos[:-1])
+        pos_totalforce[-1] += -pos[-1]
         #summing (n+1) = (n+1) - (n)
-        totalforce[1:] += (pos[:-1]-pos[1:])
-        totalforce[0] += -pos[0]
+        pos_totalforce[1:] += (pos[:-1]-pos[1:])
+        pos_totalforce[0] += -pos[0]
         #at the end of the day n = [(n) - (n+1)] + [(n) - (n-1)]
         #only at this point is total force, actually total force
-        totalforce = c*totalforce 
+        pos_totalforce = c*+pos_totalforce 
+
+        #calculate second order derivative force contribution
+
+        vel_totalforce = vel*gamma # update the force from velocity 
+        # print((vel - abs(vel - vel_totalforce) > 0).any()) false as expected
+        # contribution
+
+        totalforce = pos_totalforce+vel_totalforce 
         vel += totalforce*dt 
 
 def getPeriod(dt, c, pos, vel):
@@ -101,33 +92,25 @@ if __name__ == '__main__':# s
     L = .3 # m the length of the string (violin g)
     t0 = 0.0 # s start time
     simdt = 1e-4 # s granularity of timestep
-    # not used for opt: c = 1.5e5 # spring constant. bigger is more force
+    c = 1.5e5 # spring constant. bigger is more force
+    gamma = -1e0 # the force developed from velocity
     granularity_line = 101 # this is the number of segments to divide string
     #target for optimization
     targetFreq = 2
 
     #Plotting Twiddles
     graph_ylim = 0.05  # m 
-    framedt = simdt*100 # s granularity of video
+    framedt = simdt*500 # s granularity of video
 
     #initialization
     x = np.linspace(0, L, num=granularity_line)
     pos = (1/2 - np.abs(x-L/2)/L)/50 # who knows what this is.
     vel = np.zeros(granularity_line)
 
-    def optimizable(x):
-        '''
-        calls getPeriod with arguments from main, and will compare to targetFreq
-        returns abs
-        '''
-        return (targetFreq - getPeriod(simdt, x[0], pos.copy(), vel.copy()))**2
-
-    sol = differential_evolution(optimizable, [(1e2, 1e7)])
-    c = sol.x[0] # update c to get the target frequency
-
+    euler(framedt, simdt, c, gamma, pos, vel)
     def anim(i):
         global pos, vel
-        euler(framedt, simdt, c, pos, vel)
+        euler(framedt, simdt, c, gamma, pos, vel)
         line.set_data(x, pos)
         return line,
 
